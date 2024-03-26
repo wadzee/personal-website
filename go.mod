7eb0589cf8a8614160a2522656bb899f496b06be
radziramli <radzi@radziramli.com>
2024-03-27 07:42:18 +0800

    tidy up

--- go.mod
+++ go.mod
@@ -3,8 +3,11 @@ module github.com/wadzee/personal-website
 go 1.22.1
 
 require (
-	github.com/a-h/templ v0.2.648 // indirect
-	github.com/labstack/echo/v4 v4.11.4 // indirect
+	github.com/a-h/templ v0.2.648
+	github.com/labstack/echo/v4 v4.11.4
+)
+
+require (
 	github.com/labstack/gommon v0.4.2 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
--- view/user/show_templ.go
+++ view/user/show_templ.go
@@ -42,7 +42,7 @@ func Show(user model.User) templ.Component {
 			var templ_7745c5c3_Var3 string
 			templ_7745c5c3_Var3, templ_7745c5c3_Err = templ.JoinStringErrs(user.Email)
 			if templ_7745c5c3_Err != nil {
-				return templ.Error{Err: templ_7745c5c3_Err, FileName: `view/user/show.templ`, Line: 11, Col: 20}
+				return templ.Error{Err: templ_7745c5c3_Err, FileName: `view/user/show.templ`, Line: 11, Col: 21}
 			}
 			_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var3))
 			if templ_7745c5c3_Err != nil {
